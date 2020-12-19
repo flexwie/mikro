c6f0692dbd93e1e95f97b6c25c536184588cabae
Felix Wieland <mail@felixwie.com>
2020-12-19 10:39:57 +0100

    comon eventstream interface

---
+++ src/events/index.ts
@@ -0,0 +1,1 @@
+export { default as RedisEventStream } from './redis' 
---
+++ src/events/interface.ts
@@ -0,0 +1,31 @@
+/**
+ * Represents a callback to an event
+ */
+export type eventCallback = (error: Error | null, data: IEventData) => void
+
+/**
+ * Represents event data that is submitted with an event
+ */
+export interface IEventData {
+    emitterName: string,
+    timestamp: number,
+    data: Object
+}
+
+/**
+ * Represents an health check object
+ */
+export interface IHealthData {
+    instanceName: string,
+    serviceName: string
+}
+
+/**
+ * Represents the common interface for all event stream adapters
+ */
+export default interface IEventStream {
+    subscribe: (eventName: string, callback: eventCallback) => void
+    publish: (eventName: string, data: IEventData) => void
+    unsubscribe: (eventName: string) => void
+    health: (instanceData: IHealthData) => void
+} 
