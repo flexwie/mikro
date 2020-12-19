767f50b7105f67093f1f9ca3fff3ea892583f433
Felix Wieland <mail@felixwie.com>
2020-12-19 18:11:15 +0100

    refactoring client

---
+++ __tests__/Client.test.ts
@@ -0,0 +1,8 @@
+import Mikro, { RedisEventStream } from '../src/index'
+
+test("Mikro initiates correctly", () => {
+    let mikro = new Mikro("name", {
+        eventStream: new RedisEventStream({})
+    })
+    expect(mikro instanceof Mikro).toBeTruthy()
+})
